f1686d668ccffc3060976eeb85f1d6f9d4a407da
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-12-11 16:29:40 -0600

    Import blobs from VilleC2

--- doubleshot-vendor-blobs.mk
+++ doubleshot-vendor-blobs.mk
@@ -16,6 +16,112 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
+	vendor/htc/doubleshot/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \
+	vendor/htc/doubleshot/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \
+	vendor/htc/doubleshot/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
+	vendor/htc/doubleshot/proprietary/lib/libmmjpeg.so:obj/lib/libmmjpeg.so
+
+PRODUCT_COPY_FILES += \
+  vendor/htc/doubleshot/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
+  vendor/htc/doubleshot/proprietary/lib/libacdbmapper.so:system/lib/libacdbmapper.so \
+  vendor/htc/doubleshot/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
+  vendor/htc/doubleshot/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
+  vendor/htc/doubleshot/proprietary/lib/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+  vendor/htc/doubleshot/proprietary/bin/cand:system/bin/cand \
+  vendor/htc/doubleshot/proprietary/bin/charging:system/bin/charging \
+  vendor/htc/doubleshot/proprietary/bin/htcbatt:system/bin/htcbatt \
+  vendor/htc/doubleshot/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+  vendor/htc/doubleshot/proprietary/bin/mpdecision:system/bin/mpdecision \
+  vendor/htc/doubleshot/proprietary/bin/netmgrd:system/bin/netmgrd \
+  vendor/htc/doubleshot/proprietary/bin/netsharing:system/bin/netsharing \
+  vendor/htc/doubleshot/proprietary/bin/qmuxd:system/bin/qmuxd \
+  vendor/htc/doubleshot/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+  vendor/htc/doubleshot/proprietary/bin/thermald:system/bin/thermald \
+  vendor/htc/doubleshot/proprietary/bin/zchgd:system/bin/zchgd \
+  vendor/htc/doubleshot/proprietary/bin/awb_camera:system/bin/awb_camera \
+  vendor/htc/doubleshot/proprietary/lib/libBeautyChat.so:/system/lib/libBeautyChat.so \
+  vendor/htc/doubleshot/proprietary/lib/libcameraasd.so:/system/lib/libcameraasd.so \
+  vendor/htc/doubleshot/proprietary/lib/libcameraface.so:/system/lib/libcameraface.so \
+  vendor/htc/doubleshot/proprietary/lib/libcamerapp.so:/system/lib/libcamerapp.so \
+  vendor/htc/doubleshot/proprietary/lib/libcam_oem_plugin.so:/system/lib/libcam_oem_plugin.so \
+  vendor/htc/doubleshot/proprietary/lib/libgemini.so:/system/lib/libgemini.so \
+  vendor/htc/doubleshot/proprietary/lib/libHTC_DIS.so:/system/lib/libHTC_DIS.so \
+  vendor/htc/doubleshot/proprietary/lib/libhtccamera.so:/system/lib/libhtccamera.so \
+  vendor/htc/doubleshot/proprietary/lib/libimage-jpeg-dec-omx-comp.so:/system/lib/libimage-jpeg-dec-omx-comp.so \
+  vendor/htc/doubleshot/proprietary/lib/libimage-jpeg-enc-omx-comp.so:/system/lib/libimage-jpeg-enc-omx-comp.so \
+  vendor/htc/doubleshot/proprietary/lib/libimage-omx-common.so:/system/lib/libimage-omx-common.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_faceproc.so:/system/lib/libmmcamera_faceproc.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_frameproc.so:/system/lib/libmmcamera_frameproc.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_hdr_lib.so:/system/lib/libmmcamera_hdr_lib.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_image_stab.so:/system/lib/libmmcamera_image_stab.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_interface2.so:/system/lib/libmmcamera_interface2.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_rawchipproc.so:/system/lib/libmmcamera_rawchipproc.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_statsproc31.so:/system/lib/libmmcamera_statsproc31.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmcamera_wavelet_lib.so:/system/lib/libmmcamera_wavelet_lib.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmipl.so:/system/lib/libmmipl.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmjpeg.so:/system/lib/libmmjpeg.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmmpod.so:/system/lib/libmmmpod.so \
+  vendor/htc/doubleshot/proprietary/lib/libmmstillomx.so:/system/lib/libmmstillomx.so \
+  vendor/htc/doubleshot/proprietary/lib/liboemcamera.so:/system/lib/liboemcamera.so \
+   vendor/htc/doubleshot/proprietary/lib/libOlaEngineNew.so:/system/lib/libOlaEngineNew.so \
+  vendor/htc/doubleshot/proprietary/lib/libposteffect.so:/system/lib/libposteffect.so \
+  vendor/htc/doubleshot/proprietary/etc/agps_rm:system/etc/agps_rm \
+  vendor/htc/doubleshot/proprietary/etc/thermald.conf:system/etc/thermald.conf \
+  vendor/htc/doubleshot/proprietary/etc/calibration:system/etc/calibration \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc.b01:system/etc/firmware/vidc.b01 \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc.b02:system/etc/firmware/vidc.b02 \
+  vendor/htc/doubleshot/proprietary/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
+  vendor/htc/doubleshot/proprietary/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc.b00:system/etc/firmware/vidc.b00 \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc.b03:system/etc/firmware/vidc.b03 \
+  vendor/htc/doubleshot/proprietary/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd:system/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd \
+  vendor/htc/doubleshot/proprietary/etc/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+  vendor/htc/doubleshot/proprietary/etc/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
+  vendor/htc/doubleshot/proprietary/etc/firmware/fw_bcm4329_p2p.bin:system/etc/firmware/fw_bcm4329_p2p.bin \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
+  vendor/htc/doubleshot/proprietary/etc/firmware/default_bak.acdb:system/etc/firmware/default_bak.acdb \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidcfw.elf:system/etc/firmware/vidcfw.elf \
+  vendor/htc/doubleshot/proprietary/etc/firmware/vidc.mdt:system/etc/firmware/vidc.mdt \
+  vendor/htc/doubleshot/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+  vendor/htc/doubleshot/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+  vendor/htc/doubleshot/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+  vendor/htc/doubleshot/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+  vendor/htc/doubleshot/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+  vendor/htc/doubleshot/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
+  vendor/htc/doubleshot/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+  vendor/htc/doubleshot/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+  vendor/htc/doubleshot/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
+  vendor/htc/doubleshot/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+  vendor/htc/doubleshot/proprietary/lib/egl/libplayback_adreno200.so:system/libegl/libplayback_adreno200.so \
+  vendor/htc/doubleshot/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
+  vendor/htc/doubleshot/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
+  vendor/htc/doubleshot/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
+  vendor/htc/doubleshot/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so \
+  vendor/htc/doubleshot/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+  vendor/htc/doubleshot/proprietary/lib/libdll.so:system/lib/libdll.so \
+  vendor/htc/doubleshot/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+  vendor/htc/doubleshot/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
+  vendor/htc/doubleshot/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
+  vendor/htc/doubleshot/proprietary/lib/libhtc_rilhook.so:system/lib/libhtc_rilhook.so \
+  vendor/htc/doubleshot/proprietary/lib/libidl.so:system/lib/libidl.so \
+  vendor/htc/doubleshot/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
+  vendor/htc/doubleshot/proprietary/lib/libqc-opt.so:system/lib/libqc-opt.so \
+  vendor/htc/doubleshot/proprietary/lib/libqdp.so:system/lib/libqdp.so \
+  vendor/htc/doubleshot/proprietary/lib/libqmi.so:system/lib/libqmi.so \
+  vendor/htc/doubleshot/proprietary/lib/hw/sensors.doubleshot.so:system/lib/hw/sensors.doublehot.so \
+  vendor/htc/doubleshot/proprietary/lib/libmllite.so:system/lib/libmllite.so \
+  vendor/htc/doubleshot/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
+  vendor/htc/doubleshot/proprietary/lib/libmpl_jni.so:system/lib/libmpl_jni.so \
+  vendor/htc/doubleshot/proprietary/lib/libmpl.so:system/lib/libmpl.so \
+  vendor/htc/doubleshot/proprietary/lib/libv8.so:system/lib/libv8.so  \
+
+
+
+## IMPORT FROM ~4.4 blobs ##
+
+
+
+PRODUCT_COPY_FILES += \
 	vendor/htc/doubleshot/proprietary/lib/hw/vendor-camera.msm8660.so:obj/lib/vendor-camera.msm8660.so \
 	vendor/htc/doubleshot/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \
 	vendor/htc/doubleshot/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \
