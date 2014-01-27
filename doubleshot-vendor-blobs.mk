b325637373b3d3dfd90210d5ec9e53349bc8b914
Emmanuel Utomi <emmanuelutomi@gmail.com>
2014-01-26 20:11:12 -0600

    Update doubleshot-vendor-blobs.mk

--- doubleshot-vendor-blobs.mk
+++ doubleshot-vendor-blobs.mk
@@ -22,7 +22,7 @@ PRODUCT_COPY_FILES += \
 	vendor/htc/doubleshot/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \
 	vendor/htc/doubleshot/proprietary/lib/hw/bluetooth.default.so:obj/lib/bluetooth.default.so
 
-PRODUCT_COPY_FILES += vendor/htc/doubleshot/proprietary/app/Camera2.apk:system/app/Camera2.apk
+PRODUCT_COPY_FILES += vendor/htc/doubleshot/proprietary/app/Camera2.apk:system/app/tmp-Camera2.apk
 
 PRODUCT_COPY_FILES += \
   vendor/htc/doubleshot/proprietary/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd:system/etc/firmware/BCM4329B1_002.002.023.0589.0632.hcd \
